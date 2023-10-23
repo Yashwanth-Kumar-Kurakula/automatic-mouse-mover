7ce27a544ba4823825648443d1416b5776f97929
Yashwanth Kumar <62026836+Yashwanth-Kumar-Kurakula@users.noreply.github.com>
2023-10-23 23:45:36 +0530

    Code Enhancement

--- amm.py
+++ amm.py
@@ -1,21 +1,23 @@
-# Import the pyautogui library as 'pag' for automation tasks.
 import pyautogui as pag
-
-# Import the 'time' and 'random' libraries for controlling timing and generating random numbers.
 import time
 import random
 
-# Create an infinite loop to repeatedly execute the following actions.
+# Define constants for the range of X and Y coordinates
+X_RANGE = (600, 700)
+Y_RANGE = (200, 700)
+CLICK_INTERVAL = 30  # Time in seconds between clicks
+
+# Infinite loop to repeatedly execute the actions
 while True:
     # Generate random X and Y coordinates within the specified range.
-    x = random.randint(600, 700)
-    y = random.randint(200, 700)
-    
+    x = random.randint(*X_RANGE)
+    y = random.randint(*Y_RANGE)
+
     # Move the mouse cursor to the generated X and Y coordinates over a 1-second duration.
     pag.moveTo(x, y, 1)
-    
+
     # Simulate a mouse click at the current cursor position.
     pag.click()
-    
-    # Pause the program for 30 seconds, creating a delay before the next iteration.
-    time.sleep(30)
+
+    # Pause the program for the specified interval before the next iteration.
+    time.sleep(CLICK_INTERVAL)
